2093f702154e9978852217c60ba535c1e6cb2be0
byung-u <iam.byungwoo@gmail.com>
2017-02-13 22:45:02 +0900

    FIX with test
    Test with new logic.

--- real_estate_in_seoul/data.py
+++ real_estate_in_seoul/data.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
 
+from real_estate_in_seoul.local_code import get_local_code
 import re
 import urllib.request
 from bs4 import BeautifulSoup
-import configparser
 from datetime import datetime
 
 
-def _get_trade_price(gu, dong, apt):
+def _get_trade_price(url: str, options) -> int:
     req = urllib.request.Request(url)
     try:
         res = urllib.request.urlopen(req)
@@ -27,9 +27,9 @@ def _get_trade_price(gu, dong, apt):
         item = item.text
         item = re.sub('<.*?>', '|', item)
         info = item.split('|')
-        if info[4] != dong:
+        if info[4].startswith(options.dong) is False:
             continue
-        if info[5].find(apt) == -1:
+        if info[5].find(options.apt) == -1:
             continue
         ret_msg = '%s %s(%sm²) %s층 %s만원     준공:%s 거래:%s년%s월%s일' % (
                 info[4], info[5], info[8], info[11], info[1], info[2], info[3], info[6], info[7])
@@ -37,27 +37,31 @@ def _get_trade_price(gu, dong, apt):
 
     return 0
 
-def get_trade_price(gu='마포', dong='대흥', apt='자이'):
-    # TODO 1: module, split
-    config = configparser.ConfigParser()
-    config.readfp(open('./bot.ini'))
-    url = config.get('TOKEN', 'apt_trade_url')
-    svc_key = config.get('TOKEN', 'apt_key', raw=True)
+
+def get_trade_price(options) -> None:
+
+    local_code = get_local_code(options.gu)
+    if local_code == -1:
+        print('get_local_code falied, 서울시 %s' % options.gu)
+        return
+
     now = datetime.now()
-    year = now.year 
+    year = now.year
     month = now.month
 
-    for i in range(1, 13):
+    for i in range(0, options.month_range):
         if (month == 0):
             year -= 1
             month += 12
 
         time_str = '%4d%02d' % (year, month)
-        
-    # TODO 2: local code from 'gu' info
-        request_url = '%s?LAWD_CD=%s&DEAL_YMD=%s&serviceKey=%s' % (
-                url, 11440, time_str, svc_key)
         month = month - 1
-        _get_trade_price(request_url, gu, dong, apt)
 
+        request_url = '%s?LAWD_CD=%s&DEAL_YMD=%s&serviceKey=%s' % (
+                options.url, local_code, time_str, options.svc_key)
+        # print(request_url)
+        ret = _get_trade_price(request_url, options)
+        if ret != 0:
+            print('_get_trade_price failed, req_url=%s' % request_url)
 
+    return
