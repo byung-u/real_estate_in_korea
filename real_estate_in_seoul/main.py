327cb5d7b0d609b4f0f5e70f166f5f090054f46f
byung-u <iam.byungwoo@gmail.com>
2017-02-13 22:45:31 +0900

    FIX modulation
    Create new function for user input.

--- real_estate_in_seoul/main.py
+++ real_estate_in_seoul/main.py
@@ -1,15 +1,17 @@
 """real_estate_in_seoul trade check command line tool."""
-from real_estate_in_seoul import data
-#from local_code import is_local_exist
-#import argparse
+
+from real_estate_in_seoul.options import Options
+from real_estate_in_seoul.local_code import local_code_db_create
+from real_estate_in_seoul.data import get_trade_price
 
 import argparse
 import os
 import sys
-from typing import Any, Dict, List, Mapping, Optional
+from typing import Any, Dict, List, Mapping, Optional, Set, Tuple
 
-#def main(script_path: str) -> None:
-def main() -> None:
+
+def process_options(args: List[str]) -> Options:
+    options = Options()
     # Make the help output a little less jarring.
     help_factory = (lambda prog: argparse.RawDescriptionHelpFormatter(
         prog=prog, max_help_position=28))
@@ -26,22 +28,36 @@ def main() -> None:
     parser.add_argument(
             "-t", "--apt", metavar='apartment',
             nargs=1, help="[optional] APT")
+    parser.add_argument(
+            "-m", "--month_range", metavar='checking month range',
+            nargs=1, help="[optional] month")
 
     args = parser.parse_args()
 
     if args.gu:
-        gu = args.gu[0]
-        print(gu)
+        options.gu = args.gu[0]
 
     if args.dong:
-        dong = args.dong[0]
-        print(dong)
+        options.dong = args.dong[0]
 
     if args.apt:
-        apt = args.apt[0]
-        print(apt)
+        options.apt = args.apt[0]
 
-    get_trade_price(gu, dong, apt)
+    if args.month_range:
+        options.month_range = int(args.month_range[0])
+
+    if options.gu is None:
+        print('''MUST use -g option, use default 마포구 대흥동 자이''')
+        options.gu, options.dong, options.apt = '마포구', '대흥동', '자이'
+
+    return options
+
+
+def main() -> None:
+    options = process_options(sys.argv[1:])
+
+    local_code_db_create()  # if not exist
+
+    get_trade_price(options)
 
     sys.exit(0)
-
