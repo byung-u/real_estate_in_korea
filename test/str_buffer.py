3b58ac5e904ae81982ac7f4334f68186d2cb78f3
byung-u <iam.byungwoo@gmail.com>
2017-02-17 11:02:55 +0900

    ADD test string buffer

---
+++ test/str_buffer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.5
+# -*- coding: utf-8 -*-
+
+def main():
+    info1 = ['2012', '12', '1~10']
+    info2 = ['2017', '12', '11~20']
+    info3 = ['2016', '1', '21~31']
+    print(info1[1], info1[2][:-3])
+    print(info2[1], info2[2][:-3])
+    print(info3[1], info3[2][:-3])
+
+
+if __name__ == '__main__':
+    main()
