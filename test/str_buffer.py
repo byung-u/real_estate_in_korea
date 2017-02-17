a2043b078e3cb9ebb6facf29ec1a1019f4ac28b7
byung-u <iam.byungwoo@gmail.com>
2017-02-17 19:29:51 +0900

    ADD 1 more case

--- test/str_buffer.py
+++ test/str_buffer.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python3.5
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 def main():
     info1 = ['2012', '12', '1~10']
     info2 = ['2017', '12', '11~20']
     info3 = ['2016', '1', '21~31']
+    print(info1[1], info1[2][:3])
     print(info1[1], info1[2][:-3])
+    print('\n\n')
     print(info2[1], info2[2][:-3])
     print(info3[1], info3[2][:-3])
 
